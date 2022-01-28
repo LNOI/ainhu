feab9f49e6923222bb06d5527707cbc249ad0cdd
LNOI <19521771@gm.uit.edu.vn>
2022-01-28 23:31:41 +0700

    ainhu

--- graphic/models.py
+++ graphic/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
 # Create your models here.
+class Logs(models.Model):
+    history=models.CharField(blank=True,null=True,max_length=1000,default="x")
--- graphic/views.py
+++ graphic/views.py
@@ -1,10 +1,26 @@
+import imp
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.http import HttpResponseRedirect
+from .models import Logs
+from datetime import date, datetime
 # Create your views here.
 
 def home(request):
     return render(request,"base/index.html",{})
 
 def lixi(request):
+    if request.method=="GET":
+        if not request.GET:
+            print("Rong")
+        else:
+            if request.GET["phanthuong"]:
+                now=datetime.now()
+                dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+                log=dt_string+"    "+request.GET["phanthuong"]
+                logs={
+                    'history':log
+                }
+                Logs.objects.create(**logs)
+
     return render(request,"base/lixi.html",{})
